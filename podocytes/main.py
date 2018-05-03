5991ab9fd0ae448636589a1890e939cf13c07f12
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2018-05-03 19:24:38 +1000

    Kill javabridge after running program

--- podocytes/main.py
+++ podocytes/main.py
@@ -6,7 +6,7 @@ def main():
     fin, fout = sys.argv[1:]
     image = lifio.read_image_series(fin)
     io.imsave(fout, image)
-
+    lifio.done()  # kill javabridge, or python won't quit
 
 if __name__ == '__main__':
     main()
