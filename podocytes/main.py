e2ce2196dc6b29b44f1b0a49b1ad272adc898815
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2018-05-10 16:07:56 +1000

    Add lifio import

--- podocytes/main.py
+++ podocytes/main.py
@@ -5,6 +5,7 @@ import numpy as np
 import matplotlib as mpl
 mpl.use('wxagg')
 from skimage import io
+from . import lifio
 import matplotlib.pyplot as plt
 
 from gooey.python_bindings.gooey_decorator import Gooey as gooey
@@ -25,7 +26,10 @@ def main():
     parser.add_argument('output_folder', help='Output location',
                         widget='DirChooser')
     args = parser.parse_args()
+    lifio.start()
     print('hooray!')
+    lifio.done()
+
 
 if __name__ == '__main__':
     main()
