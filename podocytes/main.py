5790e4470e2abb35b13127e149e8236fe1912d02
GenevieveBuckley <30920819+GenevieveBuckley@users.noreply.github.com>
2018-11-08 17:16:19 +1100

    Move common utility functions to util.py file.

--- podocytes/main.py
+++ podocytes/main.py
@@ -21,6 +21,8 @@ from skimage.feature import blob_dog
 
 import tifffile._tifffile  # imported to silence pims warning
 
+from util import find_files
+
 
 def main():
     args = configure_parser()  # User input arguments
@@ -212,31 +214,6 @@ def filter_by_size(label_image, min_diameter, max_diameter):
                 (region.equivalent_diameter <= max_diameter)):
             regions.append(region)
     return regions
-
-
-def find_files(input_directory, ext):
-    """Recursive search for filenames matching specified extension.
-
-    Parameters
-    ----------
-    input_directory : str
-        Filepath to input directory location.
-    ext : str
-        File extension (eg: '.tif', '.lif', etc.)
-
-    Returns
-    -------
-    filelist : list of str
-        List of files matching specified extension in the input directory path.
-    """
-    filelist = []
-    for root, _, files in os.walk(input_directory):
-        for f in files:
-            # ignore hidden files
-            if f.endswith(ext) and not f.startswith('.'):
-                filename = os.path.join(root, f)
-                filelist.append(filename)
-    return filelist
 
 
 def find_podocytes(podocyte_image, glomeruli_region,
---
+++ podocytes/util.py
@@ -0,0 +1,23 @@
+def find_files(input_directory, ext):
+    """Recursive search for filenames matching specified extension.
+
+    Parameters
+    ----------
+    input_directory : str
+        Filepath to input directory location.
+    ext : str
+        File extension (eg: '.tif', '.lif', etc.)
+
+    Returns
+    -------
+    filelist : list of str
+        List of files matching specified extension in the input directory path.
+    """
+    filelist = []
+    for root, _, files in os.walk(input_directory):
+        for f in files:
+            # ignore hidden files
+            if f.endswith(ext) and not f.startswith('.'):
+                filename = os.path.join(root, f)
+                filelist.append(filename)
+    return filelist
